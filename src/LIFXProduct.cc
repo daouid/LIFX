2ecc7231bd39c02372fff8cefd58a9d744171194
Marius Meisenzahl <mariusmeisenzahl@gmail.com>
2015-10-18 16:50:11 +0200

    Added src/LIFXProduct.cc

---
+++ src/LIFXProduct.cc
@@ -0,0 +1,64 @@
+#include <LIFXProduct.h>
+
+LIFXProduct::LIFXProduct() {}
+
+LIFXProduct::LIFXProduct(std::string Name, std::string Identifier,
+						 std::string Company, bool HasColor,
+						 bool HasVariableColorTemp)
+{
+	this->Name = Name;
+	this->Identifier = Identifier;
+	this->Company = Company;
+	this->HasColor = HasColor;
+	this->HasVariableColorTemp = HasVariableColorTemp;
+}
+
+std::string LIFXProduct::getName()
+{
+	return Name;
+}
+
+std::string LIFXProduct::getIdentifier()
+{
+	return Identifier;
+}
+
+std::string LIFXProduct::getCompany()
+{
+	return Company;
+}
+
+bool LIFXProduct::hasColor()
+{
+	return HasColor;
+}
+
+bool LIFXProduct::hasVariableColorTemp()
+{
+	return HasVariableColorTemp;
+}
+
+void LIFXProduct::setName(std::string Name)
+{
+	this->Name = Name;
+}
+
+void LIFXProduct::setIdentifier(std::string Identifier)
+{
+	this->Identifier = Identifier;
+}
+
+void LIFXProduct::setCompany(std::string Company)
+{
+	this->Company = Company;
+}
+
+void LIFXProduct::setHastColor(bool HasColor)
+{
+	this->HasColor = HasColor;
+}
+
+void LIFXProduct::setHasVariableColorTemp(bool HasVariableColorTemp)
+{
+	this->HasVariableColorTemp = HasVariableColorTemp;
+}
